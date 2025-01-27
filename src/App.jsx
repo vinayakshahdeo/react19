4d6f827b6af28d479ce73bff3dc5b657ee32aab7
vinayakshahdeo <vinushahdeo3333@gmail.com>
2025-01-27 13:01:22 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+const App=()=> {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+	<h2>Hello React 19</h2>
+    </>
+  )
+}
+
+export default App
