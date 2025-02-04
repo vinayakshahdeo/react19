4165299275bb1254197e28e217c935d9ceb5d239
vinayakshahdeo <vinushahdeo3333@gmail.com>
2025-02-04 18:25:36 +0530

    conversion of search into search components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Search from './components/Search';
 
 const App = ()=> {
 
@@ -7,9 +8,11 @@ const App = ()=> {
     <div className='pattern'>
 		<div className="wrapper">
 		<header>
-	 <h1>Find <span className="text-gradient">Movies</span>You&apos;ll enjoy without the hassle </h1>
+			<img src='./hero.png' alt='hero banner'/>
+	 <h1>Find <span className="text-gradient">Movies</span>You&apos;ll enjoy without the hassle </h1>.
+	 3
 	 </header>
-	 <p>Search</p>
+	 <Search />
 	 </div>
     </div>
 	</main>
