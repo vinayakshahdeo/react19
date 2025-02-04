69f373c23e949a92b48923f451967c84d0b87271
vinayakshahdeo <vinushahdeo3333@gmail.com>
2025-02-04 17:21:55 +0530

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import './App.css'
 const App = ()=> {
 
   return (
-    <>
-	 <h1 className="text-3xl font-bold underline">    Hello world!  </h1>
-    </>
+	<main>
+    <div className='pattern'>
+		<div className="wrapper">
+		<header>
+	 <h1>Find <span className="text-gradient">Movies</span>You&apos;ll enjoy without the hassle </h1>
+	 </header>
+	 <p>Search</p>
+	 </div>
+    </div>
+	</main>
   )
 }
 
