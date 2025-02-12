9d39c864044826dee0b5d3b18b51ee6c6f45c0b9
vinayakshahdeo <vinushahdeo3333@gmail.com>
2025-02-12 11:46:52 +0530

    search component complete with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
+import { useState } from 'react';
 import './App.css'
 import Search from './components/Search';
 
 const App = ()=> {
-
+const [search, setSearch] = useState('')
   return (
 	<main>
     <div className='pattern'>
 		<div className="wrapper">
 		<header>
 			<img src='./hero.png' alt='hero banner'/>
-	 <h1>Find <span className="text-gradient">Movies</span>You&apos;ll enjoy without the hassle </h1>.
-	 3
+	 <h1>Find <span className="text-gradient">Movies</span>You&apos;ll enjoy without the hassle </h1>
 	 </header>
-	 <Search />
+	 <Search search={search} setSearch={setSearch}/>
+	 <h1 className="text-white">{search}</h1>
 	 </div>
     </div>
 	</main>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,9 @@
-const Search = () => {
+const Search = ({search,setSearch}) => {
   return (
-	<div>Search</div>
+	<div className="search"><div><img src="search.svg" alt="search" />
+	<input type="text" placeholder="Search through movies"
+	value={search} onChange={(event)=>setSearch(event.target.value)} /></div>
+	</div>
   )
 }
 
